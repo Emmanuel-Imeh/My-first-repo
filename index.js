932d9a83b904f88421b3e2470e10560160b360fb
Emmanuel-Imeh <davidimehemmanuel@gmail.com>
2025-01-09 03:01:20 +0100

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-a = 4
-b = 5
-x = 4 * 5
-
-document.getElementById('text').innerHTML = x;
-
-document.getElementById('index-p').innerHTML = 'Welcome Home';
+function loadPage() {
+    window.location = "galary.html";
+}
